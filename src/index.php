fda661453b5c38f5cc9e90aa2523233dfa2e9ce5
Evhenii Taranukha <tzhenia31@gmail.com>
2018-02-26 14:03:33 +0200

    edited home page

--- src/index.php
+++ src/index.php
@@ -2,25 +2,30 @@
     require_once "view/header.php";
 ?>
 
-    <div class="container">
+    <div class="container body">
         <div class="row note">
-                <div class="col-xs-12 col-sm-9 col-md-10"><a href="note.php"><span>ID: 1111</span> - 01.01.2018 (00:00) - Title one</a></div>
-                <div class="col-xs-12 col-sm-3 col-md-2">
-                    <a href="#" type="button" class="btn btn-warning">Edit</a>
-                    <a href="#" type="button" class="btn btn-danger">Delete</a>
-                </div>
+                <div class="col-xs-12"><h1>List of notes</h1></div>
         </div>
+
         <div class="row note">
             <div class="col-xs-12 col-sm-9 col-md-10"><a href="note.php"><span>ID: 1111</span> - 01.01.2018 (00:00) - Title one</a></div>
             <div class="col-xs-12 col-sm-3 col-md-2">
-                <a href="#" type="button" class="btn btn-warning">Edit</a>
+                <a href="edit.php" type="button" class="btn btn-warning">Edit</a>
+                <a href="#" type="button" class="btn btn-danger">Delete</a>
+            </div>
+        </div>
+
+        <div class="row note">
+            <div class="col-xs-12 col-sm-9 col-md-10"><a href="note.php"><span>ID: 1111</span> - 01.01.2018 (00:00) - Title one</a></div>
+            <div class="col-xs-12 col-sm-3 col-md-2">
+                <a href="edit.php" type="button" class="btn btn-warning">Edit</a>
                 <a href="#" type="button" class="btn btn-danger">Delete</a>
             </div>
         </div>
         <div class="row note">
             <div class="col-xs-12 col-sm-9 col-md-10"><a href="note.php"><span>ID: 1111</span> - 01.01.2018 (00:00) - Title one</a></div>
             <div class="col-xs-12 col-sm-3 col-md-2">
-                <a href="#" type="button" class="btn btn-warning">Edit</a>
+                <a href="edit.php" type="button" class="btn btn-warning">Edit</a>
                 <a href="#" type="button" class="btn btn-danger">Delete</a>
             </div>
         </div>
