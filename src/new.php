a5ece49804a8075dbdb50bc03b45b90cdd4a2f64
Evhenii Taranukha <tzhenia31@gmail.com>
2018-02-26 15:47:50 +0200

    added form

--- src/new.php
+++ src/new.php
@@ -9,9 +9,25 @@
 
     <div class="row">
         <div class="col-lg-12">
-            form
+            <form action="#" method="post">
+                <div class="form-group">
+                    <input type="text" class="form-control" minlength="3" maxlength="75" placeholder="Title of your note" required >
+                </div>
+
+                <div class="form-group">
+                    <textarea class="form-control"  maxlength="500"  required>111</textarea>
+                </div>
+
+                <div class="form-group">
+                    <input type="button" class="btn btn-success" value="Success">
+                </div>
+
+            </form>
         </div>
     </div>
+
+    <?php require_once "view/requirementsNotes.php"; ?>
+
 </div>
 
 <?php
