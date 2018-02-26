a96f786fab99e99cd2ab21c20d6154b88e72077d
Evhenii Taranukha <tzhenia31@gmail.com>
2018-02-26 12:33:16 +0200

    added files footer and header

---
+++ src/view/footer.php
@@ -0,0 +1,15 @@
+<footer class="container">
+    <div class="row">
+        <div class="col-lg-6">
+            Notebook 2018
+        </div>
+
+        <div class="copyright col-lg-6">
+            Developed by <a href="https://tzhenia.com" target="_blank">Yevhenii Taranukha</a>
+        </div>
+    </div>
+</footer>
+
+
+</body>
+</html>
