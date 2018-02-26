7fd90c269d85fb9dc3a57475f7d7d2b90468cc90
Evhenii Taranukha <tzhenia31@gmail.com>
2018-02-26 13:58:36 +0200

    added list of notes

--- src/index.php
+++ src/index.php
@@ -3,12 +3,29 @@
 ?>
 
     <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                11
+        <div class="row note">
+                <div class="col-xs-12 col-sm-9 col-md-10"><a href="note.php"><span>ID: 1111</span> - 01.01.2018 (00:00) - Title one</a></div>
+                <div class="col-xs-12 col-sm-3 col-md-2">
+                    <a href="#" type="button" class="btn btn-warning">Edit</a>
+                    <a href="#" type="button" class="btn btn-danger">Delete</a>
+                </div>
+        </div>
+        <div class="row note">
+            <div class="col-xs-12 col-sm-9 col-md-10"><a href="note.php"><span>ID: 1111</span> - 01.01.2018 (00:00) - Title one</a></div>
+            <div class="col-xs-12 col-sm-3 col-md-2">
+                <a href="#" type="button" class="btn btn-warning">Edit</a>
+                <a href="#" type="button" class="btn btn-danger">Delete</a>
+            </div>
+        </div>
+        <div class="row note">
+            <div class="col-xs-12 col-sm-9 col-md-10"><a href="note.php"><span>ID: 1111</span> - 01.01.2018 (00:00) - Title one</a></div>
+            <div class="col-xs-12 col-sm-3 col-md-2">
+                <a href="#" type="button" class="btn btn-warning">Edit</a>
+                <a href="#" type="button" class="btn btn-danger">Delete</a>
             </div>
         </div>
     </div>
+
 
 <?php
     require_once "view/footer.php";
