af9d58dd43caca5461e749ace772821b060fa324
Evhenii Taranukha <tzhenia31@gmail.com>
2018-02-26 15:02:14 +0200

    added label for deleted notes

--- src/note.php
+++ src/note.php
@@ -4,7 +4,7 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-lg-10"><h1>ID: 111 - Title</h1></div>
+        <div class="col-lg-10"><h1>ID: 111 - Title <small>Deleted</small></h1></div>
         <div class="col-lg-2"><p class="date text-right">01.01.2018 (00:00)</p></div>
     </div>
     <div class="row">
