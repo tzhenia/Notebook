8b4afd403e98bba72eefe155a15c787f9052af02
Evhenii Taranukha <tzhenia31@gmail.com>
2018-02-26 12:50:48 +0200

    added note page and edit view

---
+++ src/note.php
@@ -0,0 +1,19 @@
+<?php
+require_once "view/header.php";
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-10"><h1>ID: 111 - Add new note</h1></div>
+        <div class="col-lg-2"><p class="date text-right">01.01.2018 (00:00)</p></div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            form
+        </div>
+    </div>
+</div>
+
+<?php
+require_once "view/footer.php";
+?>
--- src/view/footer.php
+++ src/view/footer.php
@@ -1,10 +1,10 @@
 <footer class="container">
     <div class="row">
         <div class="col-lg-6">
-            Notebook 2018
+            Notebook
         </div>
 
-        <div class="copyright col-lg-6">
+        <div class="text-right copyright col-lg-6">
             Developed by <a href="https://tzhenia.com" target="_blank">Yevhenii Taranukha</a>
         </div>
     </div>
