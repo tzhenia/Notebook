07f725bb4cbd87ff941feb5d527f55c9de48f17b
Evhenii Taranukha <tzhenia31@gmail.com>
2018-02-26 12:14:18 +0200

    new template

---
+++ src/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="tzhenia.com">
+    <title>Notebook</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+    <header class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <ul class="nav">
+                    <li><a href="#">Notebook</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Add new</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                11
+            </div>
+        </div>
+    </div>
+
+     <footer class="container">
+         <div class="row">
+             <div class="col-lg-6">
+                 Notebook 2018
+             </div>
+
+             <div class="col-lg-6">
+                 Developed by <a href="https://tzhenia.com">Yevhenii Taranukha</a>
+             </div>
+         </div>
+     </footer>
+
+
+</body>
+</html>
