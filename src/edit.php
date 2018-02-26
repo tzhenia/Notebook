d947cb17df8c4036ff41cca25f6f062fa83147c6
Evhenii Taranukha <tzhenia31@gmail.com>
2018-02-26 13:59:31 +0200

    added edit page

---
+++ src/edit.php
@@ -0,0 +1,18 @@
+<?php
+require_once "view/header.php";
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12"><h1>Edit note</h1></div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            form
+        </div>
+    </div>
+</div>
+
+<?php
+require_once "view/footer.php";
+?>
