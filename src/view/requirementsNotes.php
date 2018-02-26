6bacdabafb84ded06995f5611fb7ab2906fe1b46
Evhenii Taranukha <tzhenia31@gmail.com>
2018-02-26 15:49:10 +0200

    added requirements for notes

---
+++ src/view/requirementsNotes.php
@@ -0,0 +1,14 @@
+<div class="row">
+    <div class="col-lg-12">
+        <h2>Requirements for note</h2>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-lg-12">
+         <ul>
+             <li>A title should consist of 3 - 50 symbols</li>
+             <li>A text should consist of 3 - 500 symbols</li>
+         </ul>
+    </div>
+</div>
