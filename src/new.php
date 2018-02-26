3fa4c537ebbc84e31f7a75936897a3ae4b774fda
Evhenii Taranukha <tzhenia31@gmail.com>
2018-02-26 12:39:44 +0200

    added page form

---
+++ src/new.php
@@ -0,0 +1,18 @@
+<?php
+require_once "view/header.php";
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12"><h1>Add new note</h1></div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            form
+        </div>
+    </div>
+</div>
+
+<?php
+require_once "view/footer.php";
+?>
