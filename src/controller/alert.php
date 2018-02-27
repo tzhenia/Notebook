36f720d0712ba63943ecb42c92add827ab792980
Evhenii Taranukha <tzhenia31@gmail.com>
2018-02-27 21:44:40 +0200

    fix bug

--- src/controller/alert.php
+++ src/controller/alert.php
@@ -6,12 +6,7 @@ if (isset($_GET['alert'])) {
         echo "<div class='row'><div class='col-xs-12'><div class='alert alert-success'>Record was saved.</div></div></div>";
     }
 
-}
-
-
-else if (isset($_GET['alert'])) {
-
-    if($_GET['alert'] == "unable"){
+    else if($_GET['alert'] == "unable"){
         echo "<div class='row'><div class='col-xs-12'><div class='alert alert-danger'>Unable to save record.</div></div></div>";
     }
 
