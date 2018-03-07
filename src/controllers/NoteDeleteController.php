b5c1864d9c38f2ef47dd3ccd256091ac16ca3a6b
Evhenii Taranukha <tzhenia31@gmail.com>
2018-03-07 15:50:50 +0200

    Added action functions

--- src/controllers/NoteDeleteController.php
+++ src/controllers/NoteDeleteController.php
@@ -5,63 +5,38 @@
  * Date: 3.2.18 (14:36)
  */
 
-
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    NoteDeleteController::hideNote($id);
-}
-
-else if (isset($_GET['delete'])) {
-    $id = $_GET['delete'];
-    NoteDeleteController::removeForever($id);
-}
-
-
-
 class NoteDeleteController{
 
     public static function actionHide($id){
 
-//        $query = "UPDATE notes SET status=:status, dateTime=:dateTime WHERE id=" . $id;
-//
-//        $status = "0";
-//        $dateTime = date('Y-m-d H:i:s');
-//
-//        $stmt = Config::get_db_connect()->prepare($query);
-//        $stmt->bindParam(':dateTime', $dateTime);
-//        $stmt->bindParam(':status', $status);
-//        //$stmt->bindParam(':id', $id);
-//
-//
-//        if($stmt->execute()){
-//            header("location: " . Config::ROOT . "/index.php?alert=saved");
-//        }
-//
-//        else{
-//            header("location: " . Config::ROOT . "/index.php?alert=unable");
-//        }
+        $checkNoteItem = Note::checkNoteById($id);
 
-        echo "hide";
+        if($checkNoteItem){
+
+            echo "hide";
+        }
+
+        else{
+            RedirectController::redirectToHome();
+        }
+
         return true;
     }
 
     public static function actionFinally($id){
 
-//        $query = "DELETE FROM notes WHERE id=" . $id;
-//        $stmt = Config::get_db_connect()->prepare($query);
-//
-//        if($stmt->execute()){
-//            header("location: " . Config::ROOT . "/index.php?alert=saved");
-//        }
-//
-//        else{
-//            header("location: " . Config::ROOT . "/index.php?alert=unable");
-//        }
+        $checkNoteItem = Note::checkNoteById($id);
 
-        echo "actionFinally";
+        if($checkNoteItem){
+
+            echo "actionFinally";
+        }
+
+        else{
+            RedirectController::redirectToHome();
+        }
+
         return true;
     }
 
 }
-
-
